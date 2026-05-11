65ae78f49459009cb0513d726a7006b326396573
Chobyeongcheon <164847365+chojo-00@users.noreply.github.com>
2026-05-11 18:21:05 +0900

    Add files via upload

--- train.sh
+++ train.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
-#SBATCH -J mtd_gan_train
+#SBATCH -J mtd_gan_train_ver1
 #SBATCH -t 7-00:00:00
 #SBATCH -o logs/%x_%A_%N.out
 #SBATCH --mail-type END,TIME_LIMIT_90,REQUEUE,INVALID_DEPEND,BEGIN
 #SBATCH --mail-user chobyeongcheon00@gmail.com
 #SBATCH -p A6000
-#SBATCH -w gpu120
+#SBATCH -w gpu122
+#SBATCH --gres=gpu:1
 
 
 # export HTTP_PROXY="http://192.168.45.108:3128"
 # export HTTPS_PROXY="http://192.168.45.108:3128"
 
 # Define vars
-JOB_NAME="mtd_gan_train"
+JOB_NAME="mtd_gan_train_ver1"
 DOCKER_IMAGE_NAME="bc_cho/${JOB_NAME}"
 DOCKER_CONTAINER_NAME="bc_cho${JOB_NAME}"
 PORT_NUM=4966
@@ -40,9 +41,17 @@ if docker ps -a -q --filter "name=${DOCKER_CONTAINER_NAME}" | grep -q .; then
     docker rm ${DOCKER_CONTAINER_NAME}
 fi
 
+# gpu 인식하냐
+echo "=== Check GPU on Host Node ==="
+nvidia-smi
+echo "CUDA_VISIBLE_DEVICES: $CUDA_VISIBLE_DEVICES"
+
+
 # Run containers
 docker run --rm \
         --name ${DOCKER_CONTAINER_NAME} \
+        --shm-size 1TB \
+        --device nvidia.com/gpu=all \
         -v /mnt/nas100/forGPU/bc_cho:/workspace/bc_cho \
         -v /mnt/nas69/ds_WBCT/IDs/cychoi/ver_1:/workspace/bc_cho/0_Project/2_LDCT2NDCT/dataset/nas69 \
         -v /mnt/nas206/ds_WBCT_share:/workspace/bc_cho/0_Project/2_LDCT2NDCT/dataset/nas206 \
